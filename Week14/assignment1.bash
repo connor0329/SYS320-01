8e11ee8b1f6e808837680a9da896f755d8e07ccb
connor0329 <connor.mccracken@mymail.champlain.edu>
2025-12-04 16:26:32 -0500

    Week 14

---
+++ Week14/assignment1.bash
@@ -0,0 +1,95 @@
+#! /bin/bash
+clear
+
+# filling courses.txt
+bash courses.bash
+
+courseFile="courses.txt"
+
+function displayCoursesofInst(){
+
+echo -n "Please Input an Instructor Full Name: "
+read instName
+
+echo ""
+echo "Courses of $instName :"
+cat "$courseFile" | grep "$instName" | cut -d';' -f1,2 | \
+sed 's/;/ | /g'
+echo ""
+
+}
+
+function courseCountofInsts(){
+
+echo ""
+echo "Course-Instructor Distribution"
+cat "$courseFile" | cut -d';' -f7 | \
+grep -v "/" | grep -v "\.\.\." | \
+sort -n | uniq -c | sort -n -r 
+echo ""
+
+}
+
+function courseLocation(){
+
+echo -n "Please input a Location: "
+read location
+
+echo ""
+echo "Courses in $location :"
+cat "$courseFile" | grep "$location" | cut -d';' -f1,2,5,6,7 | \
+sed 's/;/ | /g'
+echo ""
+
+}
+
+function courseAvailability(){
+
+echo -n "Please input a subject name: "
+read subject
+
+echo ""
+echo "Available courses in $subject :"
+
+# TODO - 1
+# Make a function that displays all the courses in given location
+# function dislaplays course code, course name, course days, time, instructor
+# Add function to the menu
+# Example input: JOYC 310
+# Example output: See the screenshots in canvas
+
+# TODO - 2
+# Make a function that displays all the courses that has availability
+# (seat number will be more than 0) for the given course code
+# Add function to the menu
+# Example input: SEC
+# Example output: See the screenshots in canvas
+
+while :
+do
+	echo ""
+	echo "Please select and option:"
+	echo "[1] Display courses of an instructor"
+	echo "[2] Display course count of instructors"
+	echo "[3] Display courses of a classroom"
+	echo "[5] Exit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "5" ]]; then
+		echo "Goodbye"
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		displayCoursesofInst
+
+	elif [[ "$userInput" == "2" ]]; then
+		courseCountofInsts
+
+	elif [[ "$userInput" == "3" ]]; then 
+		courseLocation
+
+	# TODO - 3 Display a message, if an invalid input is given
+	fi
+done
